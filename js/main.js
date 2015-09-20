9bf804e290e9160d2e24c341190d8654976512cd
Stan Devproject <stan.devproject@gmail.com>
2015-09-21 00:26:29 +0300

    Исправления и доработки

--- js/main.js
+++ js/main.js
@@ -10,45 +10,17 @@ var count_children = popup.querySelector("#jsCountChildren");
 link.addEventListener("click", function(event) {
   event.preventDefault();
   popup.classList.toggle("modal-content-show");
+
+  popup.classList.add("modal-content-animation");
+  setTimeout(function(){ popup.classList.remove("modal-content-animation"); }, 600);
+
   date_entranse.focus();
 });
 
 form.addEventListener("submit", function(event) {
   if (!date_entranse.value || !date_departure.value  || !count_adult.value) {
     event.preventDefault();
-    popup.classList.remove("modal-error");
-    popup.classList.add("modal-error");
-    setTimeout(function(){ popup.classList.remove("modal-error"); }, 650);
+    popup.classList.toggle("modal-error");
+    setTimeout(function(){ popup.classList.remove("modal-error"); }, 600);
   }
 });
-
-// window.addEventListener("keydown", function(event) {
-//   if (event.keyCode == 27) {
-//     if (popup.classList.contains("modal-content-show")) {
-//       popup.classList.remove("modal-content-show");
-//       popup.classList.remove("modal-error");
-//     }
-//   }
-// });
-
-// var mapOpen = document.querySelector(".open-map");
-// var mapPopup = document.querySelector(".modal-content-map");
-// var mapClose = mapPopup.querySelector(".modal-content-close");
-
-// mapOpen.addEventListener("click", function(event) {
-//   event.preventDefault();
-//   mapPopup.classList.add("modal-content-show");
-// });
-
-// mapClose.addEventListener("click", function(event) {
-//   event.preventDefault();
-//   mapPopup.classList.remove("modal-content-show");
-// });
-
-// window.addEventListener("keydown", function(event) {
-//   if (event.keyCode == 27) {
-//     if (mapPopup.classList.contains("modal-content-show")) {
-//       mapPopup.classList.remove("modal-content-show");
-//     }
-//   }
-// });
